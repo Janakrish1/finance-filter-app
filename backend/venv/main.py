9794e2d23e7fbc7a4d86c7a3a509157265004e08
Jana <janakrish3011@gmail.com>
2025-01-08 22:47:07 -0500

    Added the base files in frontend and backend

---
+++ backend/venv/main.py
@@ -0,0 +1,16 @@
+from flask import Flask, send_from_directory
+
+app = Flask(__name__, static_folder="../../frontend/build", static_url_path="/")
+
+# Serve the React frontend
+@app.route("/")
+def index():
+    return send_from_directory(app.static_folder, "index.html")
+
+# API or other backend routes go here (example)
+@app.route("/api/data")
+def get_data():
+    return {"message": "Hello from Flask!"}
+
+if __name__ == "__main__":
+    app.run(debug=True)  # Make sure to run in development mode for now
---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div >
+      <h1 className="text-7xl text-center text-red-400" >Financial Data Filtering App - v2</h1>
+    </div>
+  );
+}
+
+export default App;
